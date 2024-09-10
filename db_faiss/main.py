eb4075b70440f0589a3b67bcf1d1b36573318b98
Simran723-ux <simran@upcoretechnologies.com>
2024-09-11 00:36:41 +0530

    Add files via upload

--- db_faiss/main.py
+++ db_faiss/main.py
@@ -1,26 +1,3 @@
-# # main.py
-
-# import training
-# import inference
-
-# def main():
-#     # Step 1: Create and save the vector store (Training Phase)
-#     print("Starting training...")
-#     db = training.create_vector_store()  # Create the vector store
-#     db.save_local("faiss_index")  # Save FAISS index locally
-#     print("Training completed and vector store saved.")
-
-#     # Step 2: Perform inference (Inference Phase)
-#     print("Starting inference...")
-#     query = "What is Kibana?"
-#     result = inference.perform_inference(query)
-#     print("Inference result:", result)
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 # main.py
 import training
 import inference
