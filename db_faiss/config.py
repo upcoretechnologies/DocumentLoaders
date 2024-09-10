723af02708ba24ed95338c9b3b763b71cc91d265
Simran723-ux <simran@upcoretechnologies.com>
2024-09-10 13:25:23 +0530

    Update config.py

--- db_faiss/config.py
+++ db_faiss/config.py
@@ -1,10 +1,10 @@
 
 # Azure OpenAI Configuration
-OPENAI_API_KEY = "bd27e26c317a45e580662ce658d890a9"
-AZURE_OPENAI_ENDPOINT = "https://poc-slack.openai.azure.com/"
-embedding_deployment_name = "upcoretext-embedding"
-llm_deployment_name = "chatmodel"
-OPENAI_API_VERSION = "2024-05-01-preview"
+OPENAI_API_KEY = "******"
+AZURE_OPENAI_ENDPOINT = "******"
+embedding_deployment_name = "******"
+llm_deployment_name = "******"
+OPENAI_API_VERSION = "******"
 
 
 # Directory Paths
