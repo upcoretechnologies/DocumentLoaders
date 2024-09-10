787dbf5d0504c131f93110bc5519a50b0735da4e
Simran723-ux <simran@upcoretechnologies.com>
2024-09-10 13:00:32 +0530

    Update config.py

--- pymudf_loader/config.py
+++ pymudf_loader/config.py
@@ -1,11 +1,11 @@
 # config.py
 
 # Azure OpenAI Configuration
-API_KEY = "bd27e26c317a45e580662ce658d890a9"
-AZURE_OPENAI_ENDPOINT = "https://poc-slack.openai.azure.com/"
-EMBEDDING_DEPLOYMENT_NAME = "upcoretext-embedding"
-llm_deployment_name = "chatmodel"
-OPENAI_API_VERSION = "2024-05-01-preview"
+API_KEY = "*****"
+AZURE_OPENAI_ENDPOINT ="*****"
+EMBEDDING_DEPLOYMENT_NAME = "*****"
+llm_deployment_name = "*****"
+OPENAI_API_VERSION = "*****"
 
 # Directory Paths
 DOCUMENT_DIRECTORY_PATH = r'C:\DocumentLoaders\example_data'
